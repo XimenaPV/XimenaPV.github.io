dfb6486c40f8b2b933db186058d7c1decf9a5936
Ximena Puican <xpuicanvela@gmail.com>
2026-03-11 11:25:15 -0400

    added start location and first game location

--- projects/game/game.js
+++ projects/game/game.js
@@ -8,40 +8,19 @@ let gameActive = true; //this variable is required.
 
 
 //Make one function for each location
-function locationA() {
+function labStations() {
     clear();
-    print("\nYou are in location A!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
-    
-    function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
-        } else {
-            stayHere();
-            waitThenCall(locationA);
-        }
-    }
-    waitForInput(processInput);
-}
-
-function locationB() {
+    print("\You walked into the Culinary Arts classroom! Chef greets you and tells you to go change into your uniform.");
+    print("\nHead to the changing rooms:" +
+        "\n\tLocker Room");
+	}
+function lockerRoom() {
     clear();
-    print("\nYou are in location B!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
-    
-    function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
-        } else {
-            stayHere();
-            waitThenCall(locationB);
-        }
-    }
-    waitForInput(processInput);
-}
-
+    print("\nYou are in the locker room! You see some of your classmates and greet them. Then, you change into your uniform. Do not forget your hat and apron!");
+    print("\nNow that you are changed, you go to wash your hands in the bathroom.");
+    print("\nWalk to the bathroom. Take your apron before walking in." +
+        "\n\tBathroom");
+ }   
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
@@ -49,7 +28,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            labStations();
     }
     waitForInput(processInput);
 }
