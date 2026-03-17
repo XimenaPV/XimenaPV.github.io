e7571994aa4ee49c476755e4cf2838abc6190ece
Ximena Puican <xpuicanvela@gmail.com>
2026-03-17 03:28:50 -0400

    added bathroom and lab station and stage1, added some ascii drawings

--- projects/game/game.js
+++ projects/game/game.js
@@ -4,53 +4,109 @@ let gameActive = true; //this variable is required.
 //Declare your other global variables here
 let currentDay = 0;  
 let score = 0;              
-let inventory = []; 
-let bathroom = false;
-let dishRoom = false; 
-let walkInFridge = false;
-let ovenArea = false;
-let labStation = true;
 
 //If you need, add any "helper" functions here
-
-
+function printAscii(art) {
+    const output = document.getElementById('output');
+    const pre = document.createElement('pre');
+    pre.className = 'ascii-art';
+    pre.textContent = art;
+    output.appendChild(pre);
+    output.scrollTop = output.scrollHeight;
+}
 function labStations() {
     clear();
-    print("\You walked into the Culinary Arts classroom! Chef greets you and tells you to go change into your uniform.");
+    print("\nYou walked into the Culinary Arts classroom! Chef greets you and tells you to go change into your uniform.");
+    printAscii(`:-)`);
     print("\nHead to the changing rooms: \n\tLocker Room");
-	}
-function processInput(input){
-	if (input.toLowerCase() === "locker room")
+
+    function processInput(input) {
+        if (input === "locker room") {
             lockerRoom();
-	} else{
-	print(("\nChef points at the door: 'Locker room is that way!'");
-	waitForInput(processInput);}
-	{
-	waitForInput(processInput);}
+        } else {
+            waitForInput(processInput);
+        }
+    }
+    waitForInput(processInput);
 }
 
 function lockerRoom() {
     clear();
     print("\nYou are in the locker room! You see some of your classmates and greet them. Then, you change into your uniform. Do not forget your chef hat!");
-    print("\nNow that you are changed, choose whether you want to go to wash your hands in the bathroom or if you want to go pick up an apron from the lab station.");
-    print("\nWalk to the bathroom. Take your apron before walking in." +
-        "\n\tBathroom");
- }   
-function start(){
-    print("Welcome to my game! You will be in charge of baking and decorating a yellow cake! You'll have 4 days to work on your creation.  Press any key to start");
+    print("\nNow that you are changed, choose whether you want to go to <b>wash your hands in the bathroom</b> or if you want to go <b>pick up an apron from the lab station</b>.");
+    print("\n\tbathroom\n\tpick up apron");
 
-    function processInput(input){
-            labStations();
+    function processInput(input) {
+        if (input === "bathroom") {
+            bathroomO();
+        } else if (input === "pick up apron") {
+            labStationS();
+        } else {
+            waitForInput(processInput);
+        }
     }
     waitForInput(processInput);
 }
 
-function dayName(dayNumber) {
-	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday'];
-	return days =[dayNumber] || 'Finish Day!';
+function bathroomO() {
+    clear();
+    print("\nYou are in the bathroom. You can wash your hands and then go grab your apron at the lab stations to get started with the preparation.");
+    print("\nHead to the lab stations: \n\tLab Stations");
+
+    function processInput(input) {
+        if (input === "lab stations") {
+            labStationS();
+	print("\nPress <b>Enter</b> to listen to the instructions for Stage 1.");
+
+waitForInput(function() {
+                stage1(); 
+});
+       } else {
+            print("\nYou need an apron to continue the game. Go get it!");
+            waitForInput(processInput);
+        }
+    }
+    waitForInput(processInput);
 }
 
-function updateScore(points) {
-	score += points;
-	print("/n[ + " + points + "Here is your current score: " + score + "]");
-	
+function labStationS() {
+    clear();
+    print("\nYou have your apron. Now you are ready to get started! Wait here until Chef gives instructions");
+	print("\nType: wait for chef"); 
+   function processInput(input) {
+         if (input === "wait for chef") {
+             print("\nChef is getting ready to explain Stage 1.");
+             print("\nPress <b>Enter</b> to listen to the instructions.");
+             waitForInput(function() {
+                 stage1(); 
+             });
+         } else {
+             print("\nWait for Chef before you start!");
+             waitForInput(processInput);
+         }
+     }
+     waitForInput(processInput);
+ }
+
+function start() {
+    clear();
+    printAscii(`
+      _........_
+   .d888888888888b.
+  d888+"""""+888888.
+   8b' o  o  \`88888b
+ _.8P (_)    .888888
+d88P .____, .d88888P
+888b.       :88888P'
+\`"""""\`-----\`""""'
+    `);
+
+    print("Welcome to my game! You will be in charge of baking and decorating a yellow cake! You'll work in 4 stages."); print("\nPress Enter to start...");
+
+    function processInput(input) {
+        labStations();
+    }
+    
+    waitForInput(processInput);
+}
+
