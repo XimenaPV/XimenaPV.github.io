a31a1d7f005590598a12efe3d81703ec0204b3c5
Ximena Puican <xpuicanvela@gmail.com>
2026-06-16 02:35:53 -0400

    added

--- README.md
+++
@@ -1,3 +0,0 @@
-# My website
-
-This repo is my persnal portoflio website, which I am writing for ITD110, a web design class at Arlington Tech
--- js/validate.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/gracehoppercenter/validate@1.0.1/validate.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
-
